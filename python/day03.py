a52e2ed66f076af223d2ae99564e99cd9b859e8c
Niko Lockenvitz <n.lockenvitz@online.de>
2019-12-03 11:44:31 +0100

    improve Day 3 dramatically

--- python/day03.py
+++ python/day03.py
@@ -10,152 +10,62 @@ result2 = None
 # Input
 file = aoc.getFileLines()
 
-wire1 = file[0]
-wire2 = file[1]
+wire1 = file[0].split(",")
+wire2 = file[1].split(",")
 
-size = 40000
-
-def gx(x):
-    return x - size//2
-def gy(y):
-    return y - size//2
-
-def mp(x,y):
-    return m[y+size//2][x+size//2]
+d = {}
 
 # Implementation
-def go(pos, instruction, wire):
+def go(x, y, instruction, wire, steps):
     direction = instruction[0]
     distance = int(instruction[1:])
-    crossings = []
-    if (direction == "U" or direction == "D"):
-        if (direction == "U"):
-            d = -distance - 1
-            s = -1
-        elif (direction == "D"):
-            d = distance + 1
-            s = 1
-        for y in range(pos[1],pos[1]+d,s):
-            if (wire == 2 and mp(pos[0],y) == 1):
-                crossings.append([pos[0],y])
-            if (mp(pos[0],y) & wire == 0):
-                m[gy(y)][gx(pos[0])] += wire
-        pos = [pos[0],pos[1]+d-s]
-    elif (direction == "R" or direction == "L"):
-        if (direction == "R"):
-            d = distance + 1
-            s = 1
-        elif (direction == "L"):
-            d = -distance - 1
-            s = -1
-        for x in range(pos[0],pos[0]+d,s):
-            if (wire == 2 and mp(x,pos[1]) == 1):
-                crossings.append([x,pos[1]])
-            if (mp(x,pos[1]) & wire == 0):
-                m[gy(pos[1])][gx(x)] += wire
-        pos = [pos[0]+d-s,pos[1]]
-    return [pos, crossings]
+    crosses = []
+    cx, cy = x, y
+    for i in range(distance):
+        if (direction == "U"): cy += 1
+        if (direction == "R"): cx += 1
+        if (direction == "D"): cy -= 1
+        if (direction == "L"): cx -= 1
+        steps += 1
 
-def md(point):
+        if((cx,cy) in d):
+            if(d[(cx,cy)][wire-1] == 0):
+                d[(cx,cy)][wire-1] = steps
+            if(d[(cx,cy)][1-(wire-1)] > 0):
+                crosses.append((cx,cy))
+        else:
+            d[(cx,cy)] = [0,0]
+            d[(cx,cy)][wire-1] = steps
+
+    return [cx, cy, steps, crosses]
+
+def m1(point):
     return abs(point[0]) + abs(point[1])
-    
-def part1():
-    result = 0
-    pos = [0,0]
-    cross = []
-    for instruction in wire1.split(","):
-        [newpos, crossings] = go(pos, instruction, 1)
-        pos[0],pos[1] = newpos[0],newpos[1]
-    pos = [0,0]
-    for instruction in wire2.split(","):
-        [newpos, crossings] = go(pos, instruction, 2)
-        pos[0],pos[1] = newpos[0],newpos[1]
-        for el in crossings:
-            if (md(el) > 0):
-                if (cross == []):
-                    cross = [el[0],el[1]]
-                elif (md(cross) > md(el)):
-                    cross = [el[0],el[1]]
-    return md(cross)
 
-def go2(pos, instruction, wire, steps):
-    direction = instruction[0]
-    distance = int(instruction[1:])
-    crossings = []
-    steps -= 1
-    if (direction == "U" or direction == "D"):
-        if (direction == "U"):
-            d = -distance - 1
-            s = -1
-        elif (direction == "D"):
-            d = distance + 1
-            s = 1
-        for y in range(pos[1],pos[1]+d,s):
-            steps+=1
-            if (wire == 2 and mp(pos[0],y)[0] > 0):
-                crossings.append([pos[0],y])
-            if (mp(pos[0],y)[wire-1] == 0):
-                m[gy(y)][gx(pos[0])][wire-1] = steps
-        pos = [pos[0],pos[1]+d-s]
-    elif (direction == "R" or direction == "L"):
-        if (direction == "R"):
-            d = distance + 1
-            s = 1
-        elif (direction == "L"):
-            d = -distance - 1
-            s = -1
-        for x in range(pos[0],pos[0]+d,s):
-            steps+=1
-            if (wire == 2 and mp(x,pos[1])[0] > 0):
-                crossings.append([x,pos[1]])
-            if (mp(x,pos[1])[wire-1] == 0):
-                m[gy(pos[1])][gx(x)][wire-1] = steps
-        pos = [pos[0]+d-s,pos[1]]
-    return [pos, crossings, steps]
+def m2(point):
+    return sum(d[(point[0],point[1])])
 
-def ms(point):
-    return sum(mp(point[0],point[1]))
-
-def part2():
-    result = 0
-    pos = [0,0]
-    cross = []
-    steps = 0
-    for instruction in wire1.split(","):
-        [newpos, crossings, newsteps] = go2(pos, instruction, 1, steps)
-        pos[0],pos[1] = newpos[0],newpos[1]
-        steps = newsteps
-    pos = [0,0]
-    steps = 0
-    for instruction in wire2.split(","):
-        [newpos, crossings, newsteps] = go2(pos, instruction, 2, steps)
-        pos[0],pos[1] = newpos[0],newpos[1]
-        steps = newsteps
-        for el in crossings:
-            if (el[0] > 0 and el[1] > 0):
-                if (cross == []):
-                    cross = [el[0],el[1]]
-                elif (ms(cross) > ms(el)):
-                    cross = [el[0],el[1]]
-                    print(ms(cross))
-        if(steps > ms(cross)):
-            break
-    return ms(cross)
+def part1n2():
+    result1, result2 = 0, 0
+    cross1, cross2 = [], []
+    x,y, steps = 0, 0, 0
+    for instruction in wire1:
+        [x, y, steps, crosses] = go(x, y, instruction, 1, steps)
+    x,y, steps = 0, 0, 0
+    for instruction in wire2:
+        [x, y, steps, crosses] = go(x, y, instruction, 2, steps)
+        for el in crosses:
+            if(el[0] == 0 and el[1] == 0): continue
+            if (cross1 == [] or m1(el) < m1(cross1)):
+                cross1 = [el[0],el[1]]
+            if (cross2 == [] or m2(el) < m2(cross2)):
+                cross2 = [el[0],el[1]]
+    result1 = m1(cross1)
+    result2 = m2(cross2)
+    return [result1, result2]
 
 # Processing
-#m = []
-#for i in range(size):
-#    m.append([0]*size)
-
-#result1 = part1()
-
-m = []
-for i in range(size):
-    m.append([])
-    for j in range(size):
-        m[i].append([0,0])
-
-result2 = part2()
+[result1, result2] = part1n2()
 
 # Output
 aoc.output(result1, result2)
